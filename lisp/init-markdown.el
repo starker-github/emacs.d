a42b23994ecdc01e96c2f73547f0e58c0fcefdfd
Jiang Tao <tao.jiang@ingenic.com>
2015-05-19 14:38:17 +0800

    1. tab-width is 8 in markdown-mode.

--- lisp/init-markdown.el
+++ lisp/init-markdown.el
@@ -3,4 +3,8 @@
 (setq auto-mode-alist
       (cons '("\\.\\(md\\|markdown\\)\\'" . markdown-mode) auto-mode-alist))
 
+(add-hook 'markdown-mode-hook (lambda ()
+                                (setq tab-width 8) ;;8个缩进
+                                ))
+
 (provide 'init-markdown)
